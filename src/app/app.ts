876109d421ddfcd47ef510735699151c7fbcae23
Aleksander Ontin <aleksander.ontin@gmail.com>
2026-06-19 22:20:16 +0300

    introduce global navbar and footer with base layout structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FullscreenService } from './core/services/fullscreen.service';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
